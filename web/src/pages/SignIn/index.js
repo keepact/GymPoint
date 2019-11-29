25cae0debf389d181c416c1fb9e85cf65e3b6841
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-11-29 00:00:45 -0300

    Login page styles

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import logo from '~/assets/logo.svg';
+
 import { Container } from './styles';
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <img src={logo} alt="GymPoint" />
+
+      <form>
+        <label>Seu Email</label>
+        <input type="email" placeholder="exemplo@email.com" />
+        <label>Sua Senha</label>
+        <input type="password" placeholder="*****" />
+
+        <button type="submit">Acessar</button>
+      </form>
+    </>
+  );
 }
--- web/src/pages/_layouts/auth/styles.js
+++ web/src/pages/_layouts/auth/styles.js
@@ -1,6 +1,61 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
   background: #ee4d64;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 315px;
+  text-align: center;
+  background: #fff;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  padding: 40px 20px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    label {
+      color: #444;
+      font-size: 14px;
+      font-weight: bold;
+      text-align: left;
+      margin-bottom: 10px;
+      text-transform: uppercase;
+    }
+
+    input {
+      background: #fff;
+      border-radius: 4px;
+      border: solid 1px #dddddd;
+      height: 44px;
+      padding: 0 15px;
+      color: #999;
+      margin: 0 0 10px;
+    }
+
+    button {
+      margin: 5px 0 0;
+      height: 44px;
+      background: #ee4d64;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      font-weight: bold;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.08, '#ee4d64')};
+      }
+    }
+  }
 `;
