b199c03315c29cf0e62e48ceda6cfd13f1d2f8aa
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-11-29 11:06:04 -0300

    Yup validation

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import * as Yup from 'yup';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
+
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GymPoint" />
 
-      <form>
-        <label htmlFor="email">
-          <span>Seu Email</span>
-          <input name="email" type="email" placeholder="exemplo@email.com" />
-        </label>
-        <label htmlFor="password">
-          <span>Sua Senha</span>
-          <input name="password" type="password" />
-        </label>
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <label htmlFor="email">Seu Email</label>
+        <Input name="email" type="email" placeholder="exemplo@email.com" />
+        <label htmlFor="password">Sua Senha</label>
+        <Input name="password" type="password" />
 
         <button type="submit">Acessar</button>
-      </form>
+      </Form>
     </>
   );
 }
