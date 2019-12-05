0697e7d8aef8be4f265274b8567b485123275cd9
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-12-05 09:00:42 -0300

    Page to register students and styles

--- web/src/pages/Plans/List/index.js
+++ web/src/pages/Plans/List/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { formatPrice } from '~/util';
+import { formatPrice } from '~/utils';
 
 import api from '~/services/api';
 
--- web/src/pages/Students/Create/index.js
+++ web/src/pages/Students/Create/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Content, TitleWrapper } from '~/components/Container';
+import { Form, NumberInputs } from './styles';
 
 export default function Create() {
-  return <div />;
+  return (
+    <Container>
+      <TitleWrapper>
+        <h1>Cadastro de Aluno</h1>
+        <button type="button">Voltar</button>
+        <button type="button">Salvar</button>
+      </TitleWrapper>
+      <Content>
+        <Form>
+          <label htmlFor="">Nome Completo </label>
+          <input type="text" placeholder="John Doe" />
+          <label htmlFor="">Endereço de Email</label>
+          <input type="text" placeholder="example@email.com" />
+
+          <NumberInputs>
+            <div>
+              <label htmlFor="">Idade</label>
+              <input type="number" />
+            </div>
+            <div>
+              <label htmlFor="">PESO (em kg)</label>
+              <input type="number" />
+            </div>
+            <div>
+              <label htmlFor="">Altura</label>
+              <input type="number" />
+            </div>
+          </NumberInputs>
+        </Form>
+      </Content>
+    </Container>
+  );
 }
