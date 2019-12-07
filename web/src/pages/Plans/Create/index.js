089f956b55bf745f5f232080bc4a8bf9238b6874
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-12-07 11:23:26 -0300

    Style optimization plans and registration pages

--- web/src/pages/Plans/Create/index.js
+++ web/src/pages/Plans/Create/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  Content,
+  ContainerForm,
+  Form,
+  NumberInputs,
+  TitleWrapper,
+} from '~/components/Container';
 
 export default function Create() {
-  return <div />;
+  return (
+    <ContainerForm>
+      <TitleWrapper>
+        <h1>Cadastro de Aluno</h1>
+        <div>
+          <button type="button">Voltar</button>
+          <button type="button">Salvar</button>
+        </div>
+      </TitleWrapper>
+      <Content>
+        <Form>
+          <label htmlFor="">Título do Plano</label>
+          <input type="text" placeholder="John Doe" />
+          <NumberInputs>
+            <div>
+              <label htmlFor="">Duração (em meses)</label>
+              <input type="number" />
+            </div>
+            <div>
+              <label htmlFor="">Preço Mensal</label>
+              <input type="number" />
+            </div>
+            <div>
+              <label htmlFor="">Preço Total</label>
+              <input className="gray" type="text" readOnly />
+            </div>
+          </NumberInputs>
+        </Form>
+      </Content>
+    </ContainerForm>
+  );
 }
--- web/src/pages/Registration/Create/index.js
+++ web/src/pages/Registration/Create/index.js
@@ -1,7 +1,47 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  Content,
+  ContainerForm,
+  Form,
+  NumberInputs,
+  TitleWrapper,
+} from '~/components/Container';
 
 export default function Create() {
-  return <div />;
+  return (
+    <ContainerForm>
+      <TitleWrapper>
+        <h1>Cadastro de Matrícula</h1>
+        <div>
+          <button type="button">Voltar</button>
+          <button type="button">Salvar</button>
+        </div>
+      </TitleWrapper>
+      <Content>
+        <Form>
+          <label htmlFor="">Aluno</label>
+          <input type="text" placeholder="John Doe" />
+          <NumberInputs columns>
+            <div>
+              <label htmlFor="">Plano</label>
+              <input type="number" />
+            </div>
+            <div>
+              <label htmlFor="">Data de ínicio</label>
+              <input type="number" />
+            </div>
+            <div>
+              <label htmlFor="">Data de Termíno</label>
+              <input className="gray" type="text" readOnly />
+            </div>
+            <div>
+              <label htmlFor="">Valor Final</label>
+              <input className="gray" type="text" readOnly />
+            </div>
+          </NumberInputs>
+        </Form>
+      </Content>
+    </ContainerForm>
+  );
 }
