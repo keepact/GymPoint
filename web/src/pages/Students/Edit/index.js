29a5bc0329eeabe7192919ad9ea0adb154f12c42
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-12-19 08:44:48 -0300

    Converting numbers to decimals and then to string

--- web/src/pages/Students/Edit/index.js
+++ web/src/pages/Students/Edit/index.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
+import { Input } from '@rocketseat/unform';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+import InputNumber from '~/components/NumberInput';
 
 import api from '~/services/api';
 
@@ -17,11 +22,45 @@ function Edit({ match }) {
   const [loading, setLoading] = useState(true);
   const [student, setStudent] = useState('');
 
+  function parseDecimal(numberVal, type) {
+    if (numberVal < 100 && type === 'weight') {
+      return ((numberVal / 100) * 100).toFixed(2);
+    }
+    if (numberVal < 100 && type === 'height') {
+      return (numberVal / 10).toFixed(2);
+    }
+    return (numberVal / 100).toFixed(2);
+  }
+
   async function loadData() {
     const response = await api.get(`/students/${match.params.id}`);
 
-    setStudent(response.data);
+    setStudent({
+      ...response.data,
+      weight_formatted: parseDecimal(response.data.weight, 'weight'),
+      height_formatted: parseDecimal(response.data.height, 'height'),
+    });
     setLoading(false);
+  }
+
+  async function handleSubmit(data) {
+    try {
+      const { weight_formatted, height_formatted } = data;
+      const weight = String(weight_formatted).replace(/[^0-9|-]/g, '');
+      const height = String(height_formatted).replace(/[^0-9|-]/g, '');
+
+      await api.put(`/students/${match.params.id}`, {
+        name: data.name,
+        email: data.email,
+        age: data.age,
+        weight,
+        height,
+      });
+      toast.success('Cadastro Atualizado');
+    } catch (err) {
+      console.log(err);
+      toast.error('Erro na requisição');
+    }
   }
 
   useEffect(() => {
@@ -35,31 +74,37 @@ function Edit({ match }) {
         <h1>Edição de aluno</h1>
         <div>
           <Link to="/">Voltar</Link>
-          <button type="button">Salvar</button>
+          <button form="Form" type="submit">
+            Salvar
+          </button>
         </div>
       </TitleWrapper>
       <Content>
-        <MyForm>
+        <MyForm id="Form" initialData={student} onSubmit={handleSubmit}>
           <>
             <label htmlFor="">Nome Completo</label>
-            <input type="text" placeholder={student.name} />
+            <Input name="name" />
             <label htmlFor="">Endereço de Email</label>
-            <input type="text" placeholder={student.email} />
+            <Input name="email" />
 
             <NumberInputs>
               <div>
                 <label htmlFor="">Idade</label>
-                <input type="number" placeholder={student.age} />
+                <Input name="age" type="number" />
               </div>
               <div>
                 <label htmlFor="">
                   Peso <span>(em kg)</span>
                 </label>
-                <input type="number" placeholder={student.weight} />
+                <InputNumber decimalScale={2} name="weight_formatted" />
               </div>
               <div>
                 <label htmlFor="">Altura</label>
-                <input type="number" placeholder={student.height} />
+                <InputNumber
+                  thousandSeparator
+                  decimalScale={2}
+                  name="height_formatted"
+                />
               </div>
             </NumberInputs>
           </>
