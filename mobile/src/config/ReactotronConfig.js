0f109e610c21153122b6312f8abfece79e1461e1
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2020-01-18 20:13:52 -0300

    Fix Reactotron config and add root import plugin

--- mobile/src/config/ReactotronConfig.js
+++ mobile/src/config/ReactotronConfig.js
@@ -5,7 +5,7 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
   const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
-    .configure({ host: '192.168.0.17' })
+    .configure({ host: '192.168.0.11', port: 9090 })
     .useReactNative()
     .use(reactotronRedux())
     .use(reactotronSaga())
--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-    </View>
-  );
+  return <View />;
 };
 
 export default App;
