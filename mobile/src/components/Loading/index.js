40fb700e2d594ae3dfb666ff025bcf8d05bfbd08
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2020-02-02 14:10:44 -0300

    Loading component proptypes

--- mobile/src/components/Loading/index.js
+++ mobile/src/components/Loading/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { View } from 'react-native';
 
@@ -9,26 +10,42 @@ import {
   Loader,
 } from 'rn-placeholder';
 
-export default ({ left = false, bottom = true, style = {} }) => (
-  <Placeholder
-    Animation={Loader}
-    style={style}
-    Left={left ? PlaceholderMedia : null}>
-    <PlaceholderLine width={50} height={20} />
-    <PlaceholderLine height={10} />
-    <PlaceholderLine height={14} />
-    <PlaceholderLine height={14} />
+function Loading({ left, bottom, style }) {
+  return (
+    <Placeholder
+      Animation={Loader}
+      style={style}
+      Left={left ? PlaceholderMedia : null}>
+      <PlaceholderLine width={50} height={20} />
+      <PlaceholderLine height={10} />
+      <PlaceholderLine height={14} />
+      <PlaceholderLine height={14} />
 
-    {bottom && (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}>
-        <PlaceholderLine width={30} />
-        <PlaceholderLine width={30} />
-      </View>
-    )}
-  </Placeholder>
-);
+      {bottom && (
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+          <PlaceholderLine width={30} />
+          <PlaceholderLine width={30} />
+        </View>
+      )}
+    </Placeholder>
+  );
+}
+
+Loading.defaultProps = {
+  left: false,
+  bottom: true,
+  style: {},
+};
+
+Loading.propTypes = {
+  left: PropTypes.bool,
+  bottom: PropTypes.bool,
+  style: PropTypes.oneOfType([PropTypes.object]),
+};
+
+export default Loading;
