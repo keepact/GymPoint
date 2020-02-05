1b77abcb809de1765e63ff34265a56fe50edb871
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2020-02-04 23:21:36 -0300

    Service pattern first step

--- backend/src/app/controllers/PendingController.js
+++ backend/src/app/controllers/PendingController.js
@@ -1,56 +1,13 @@
-import { Op } from 'sequelize';
-import Registration from '../models/Registration';
-
-import Student from '../models/Student';
-import Plan from '../models/Plan';
+import PendingService from '../services/PendingService';
 
 class PendingController {
   async index(req, res) {
+    const result = await new PendingService().index();
     const { page } = req.query;
 
-    let limit = {};
+    const lastPage = page * 10 >= result.count;
 
-    if (page) {
-      limit = {
-        limit: 10,
-        offset: (page - 1) * 10,
-      };
-    }
-
-    const registrations = await Registration.findAndCountAll({
-      ...limit,
-      attributes: ['id', 'start_date', 'end_date', 'price', 'active'],
-      include: [
-        {
-          model: Student,
-          as: 'student',
-          attributes: ['name'],
-        },
-        {
-          model: Plan,
-          as: 'plan',
-          attributes: ['title'],
-        },
-      ],
-      where: {
-        [Op.or]: [
-          {
-            plan_id: {
-              [Op.eq]: null,
-            },
-          },
-          {
-            student_id: {
-              [Op.eq]: null,
-            },
-          },
-        ],
-      },
-    });
-
-    const lastPage = page ? page * limit.limit >= registrations.count : true;
-
-    return res.json({ content: registrations, lastPage });
+    return res.status(result ? 200 : 400).json({ content: result, lastPage });
   }
 }
 
