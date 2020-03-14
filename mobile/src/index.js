15fae50f3aab3d415d3ff20c56d4eaf8a0474d67
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2020-03-14 12:46:27 -0300

    Configuring pusher to receive real-time messages from the web app

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -3,6 +3,8 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 
+import { configurePusher } from 'pusher-redux/react-native';
+
 import './config/ReactotronConfig';
 
 import { store, persistor } from './store';
@@ -10,8 +12,16 @@ import App from './App';
 
 import Header from '~/components/Header';
 import Background from '~/components/Background';
+import { PUSHER_KEY } from 'react-native-dotenv';
 
 export default function Index() {
+  const options = {
+    cluster: 'us2',
+    useTLS: true,
+  };
+
+  configurePusher(store, PUSHER_KEY, options);
+
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
